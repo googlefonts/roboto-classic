a28121936b4606bb556a325ccb1abc4dca03361a
sannorozco <santiagorozco@gmail.com>
2020-02-12 10:15:47 -0600

    Update build-min.sh
    —with atomated ttx remove MVAR each build

--- build-min.sh
+++ build-min.sh
@@ -1,2 +1,9 @@
 fontmake -o ttf-interpolatable -m Roboto-min.designspace --no-production-names
 fonttools varLib Roboto-min.designspace
+
+ttx -x MVAR Roboto-min-VF.ttf
+rm Roboto-min-VF.ttf
+ttx Roboto-min-VF.ttx
+rm Roboto-min-VF.ttx
+
+mv Roboto-min-VF.ttf fonts/Roboto-min-VF.ttf
