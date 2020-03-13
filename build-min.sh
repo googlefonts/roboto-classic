80c795db140befabd873b05c45e8d67211528af0
sannorozco <santiagorozco@gmail.com>
2020-03-12 18:23:06 -0600

    Update VF
    —decomposed flipped ["uni0313", "uni0315", "uni1DCE", "uni1DE0", "uni1DEE", "emdash", "uni2094", "uni2DEC", "uni2E0D", "uni2E15", "uni2E1D", "uni2E32", "uni2E3A", "uni2E3B", "uniA780", "uniAB51", "commaaboverightcomb"]
    —update build to extract hints via vttlib, then merge them into the new generated VF
    —NOT compiled yet, there’s a bug

--- build-min.sh
+++ build-min.sh
@@ -7,3 +7,11 @@ ttx Roboto-min-VF.ttx
 rm Roboto-min-VF.ttx
 
 mv Roboto-min-VF.ttf fonts/Roboto-min-VF.ttf
+
+python -m vttLib dumpfile Hinted\ VTT/MD\ Hints/Sources/VTTSourceRoboto-min-VF.ttf
+
+mv Hinted\ VTT/MD\ Hints/Sources/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx
+
+python -m vttLib mergefile fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/Roboto-min-VF.ttf
+
+python -m vttLib compile fonts/Roboto-min-VF.ttf
