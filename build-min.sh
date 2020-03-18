fe14657c7783647ac3d14208a2d1c658c585b8c5
sannorozco <santiagorozco@gmail.com>
2020-03-18 11:58:11 -0600

    Update build-min.sh

--- build-min.sh
+++ build-min.sh
@@ -1,28 +1,44 @@
+
+#Make VF
 fontmake -o ttf-interpolatable -m Roboto-min.designspace --no-production-names
 fonttools varLib Roboto-min.designspace
+mv Roboto-min-VF.ttf fonts/Roboto-unhinted.ttf
 
-ttx -x MVAR Roboto-min-VF.ttf
-rm Roboto-min-VF.ttf
-ttx Roboto-min-VF.ttx
-rm Roboto-min-VF.ttx
+# Remove MVAR
+ttx -x MVAR fonts/Roboto-unhinted.ttf
+rm fonts/Roboto-unhinted.ttf
+ttx fonts/Roboto-unhinted.ttx
+rm fonts/Roboto-unhinted.ttx
 
-mv Roboto-min-VF.ttf fonts/Roboto-min-VF.ttf
+# Merge Hints from VTT source
+python -m vttLib dumpfile HintingSource/VTTSourceRoboto-min-VF.ttf
 
-python -m vttLib dumpfile Hinted\ VTT/MD\ Hints/Sources/RobotoVTTSources/VTTSourceRoboto-min-VF.ttf
+mv HintingSource/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx
 
-mv Hinted\ VTT/MD\ Hints/Sources/RobotoVTTSources/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx
-
-python -m vttLib mergefile fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/Roboto-min-VF.ttf
+python -m vttLib mergefile fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx fonts/Roboto-unhinted.ttf
 
 rm fonts/VTTSourceRoboto-min-VF_VTT_Hinting.ttx
 
-python -m vttLib compile fonts/Roboto-min-VF.ttf
+python -m vttLib compile fonts/Roboto-unhinted.ttf
 
-rm fonts/Roboto-min-VF.ttf
+rm fonts/Roboto-unhinted.ttf
 
-mv fonts/Roboto-min-VF#1.ttf fonts/Roboto-min-VF.ttf
+mv fonts/Roboto-unhinted#1.ttf fonts/Roboto[ital,wdth,wght].ttf
 
-ttx -x TSI1 -x TSI3 -x TSI5 fonts/Roboto-min-VF.ttf
-rm fonts/Roboto-min-VF.ttf
-ttx fonts/Roboto-min-VF.ttx
-rm fonts/Roboto-min-VF.ttx
+#fix maxp
+ttx -m fonts/Roboto[ital,wdth,wght].ttf fonts/fixes/maxp-fix.ttx
+mv fonts/fixes/maxp-fix.ttf fonts/maxp-fix.ttf
+ttx fonts/maxp-fix.ttf
+rm fonts/maxp-fix.ttf
+rm fonts/Roboto[ital,wdth,wght].ttf
+ttx fonts/maxp-fix.ttx
+rm fonts/maxp-fix.ttx
+mv fonts/maxp-fix.ttf fonts/Roboto[ital,wdth,wght].ttf
+
+#fix STAT
+
+#remove TSI tables from VF
+ttx -x TSI0 -x TSI1 -x TSI2 -x TSI3 -x TSI5 fonts/Roboto[ital,wdth,wght].ttf
+rm fonts/Roboto[ital,wdth,wght].ttf
+ttx fonts/Roboto[ital,wdth,wght].ttx
+rm fonts/Roboto[ital,wdth,wght].ttx
