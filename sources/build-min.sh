4df0100f7c3aab9c158e49c200faf2b60ac3b0ab
Marc Foley <m.foley.88@gmail.com>
2020-04-08 15:00:24 +0100

    Tidyup vtt steps

--- sources/build-min.sh
+++ sources/build-min.sh
@@ -5,10 +5,9 @@ fontmake -m sources/Roboto-min.designspace -o variable --output-path fonts/Robot
 # Remove MVAR
 gftools fix-unwanted-tables -t "MVAR" fonts/Roboto[ital,wdth,wght].ttf
 
-# Merge Hints from VTT source
+# Transfer Hints and compile them
 python -m vttLib mergefile sources/vtt-hinting.ttx fonts/Roboto[ital,wdth,wght].ttf
-python -m vttLib compile fonts/Roboto[ital,wdth,wght].ttf
-rm fonts/Roboto[ital,wdth,wght].ttf
+python -m vttLib compile fonts/Roboto[ital,wdth,wght].ttf --ship
 mv fonts/Roboto[ital,wdth,wght]#1.ttf fonts/Roboto[ital,wdth,wght].ttf
 
 # Add gasp table
@@ -16,11 +15,6 @@ python Scripts/fix_gasp.py fonts/Roboto[ital,wdth,wght].ttf
 
 # Fix STAT
 statmake --designspace sources/Roboto-min.designspace --stylespace sources/Roboto-min.stylespace fonts/Roboto[ital,wdth,wght].ttf
-
-# Compile hints
-python -m vttLib compile fonts/Roboto[ital,wdth,wght].ttf --ship
-rm fonts/Roboto[ital,wdth,wght].ttf
-mv fonts/Roboto[ital,wdth,wght]#1.ttf fonts/Roboto[ital,wdth,wght].ttf
 
 # Make web
 mkdir -p fonts/web
