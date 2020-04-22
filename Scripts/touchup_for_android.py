f0fba09724a4970a2219edccee15687b05582e3c
Marc Foley <m.foley.88@gmail.com>
2020-04-22 15:41:46 +0100

    remove redundant glyphs from android build

--- Scripts/touchup_for_android.py
+++ Scripts/touchup_for_android.py
@@ -1,9 +1,11 @@
 import sys
 from fontTools.ttLib import TTFont
+from nototools import font_data
 
 
 YMIN = -555
 YMAX = 2163
+
 
 
 def main(font_path):
@@ -20,6 +22,19 @@ def main(font_path):
         else:
             font["OS/2"].fsSelection ^= 64 | 32
         font["head"].macStyle |= 1
+
+    # turn off round-to-grid flags in certain problem components
+    # https://github.com/google/roboto/issues/153
+    glyph_set = font.getGlyphSet()
+    ellipsis = glyph_set['ellipsis']._glyph
+    for component in ellipsis.components:
+        component.flags &= ~(1 << 2)
+
+    font_data.delete_from_cmap(font, [
+        0x20E3, # COMBINING ENCLOSING KEYCAP
+        0x2191, # UPWARDS ARROW
+        0x2193, # DOWNWARDS ARROW
+        ])
     font.save(font_path)
 
 
