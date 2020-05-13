924f0fddf6d2887b2581e50e07b7f4c03abd9904
Marc Foley <m.foley.88@gmail.com>
2020-05-13 15:19:43 +0100

    rename Scripts to scripts. OS X fix

--- sources/build.sh
+++ sources/build.sh
@@ -7,20 +7,20 @@ mkdir -p fonts
 mkdir -p fonts/unhinted
 UNHINTED_VF_PATH=fonts/unhinted/Roboto[ital,wdth,wght].ttf
 fontmake -m sources/Roboto.designspace -o variable --output-path $UNHINTED_VF_PATH
-python Scripts/drop_mvar.py $UNHINTED_VF_PATH
+python scripts/drop_mvar.py $UNHINTED_VF_PATH
 statmake --designspace sources/Roboto.designspace --stylespace sources/Roboto.stylespace $UNHINTED_VF_PATH
-python Scripts/instantiate_statics.py $UNHINTED_VF_PATH fonts/unhinted/static
+python scripts/instantiate_statics.py $UNHINTED_VF_PATH fonts/unhinted/static
 
 
 # Make Android
 mkdir -p fonts/android
 ANDROID_VF_PATH=fonts/android/Roboto[ital,wdth,wght].ttf
 subset.py $UNHINTED_VF_PATH $ANDROID_VF_PATH
-python Scripts/touchup_for_android.py $ANDROID_VF_PATH
-python Scripts/instantiate_statics.py $ANDROID_VF_PATH fonts/android/static
+python scripts/touchup_for_android.py $ANDROID_VF_PATH
+python scripts/instantiate_statics.py $ANDROID_VF_PATH fonts/android/static
 for font in $(ls fonts/android/static/*.ttf)
 do
-	python Scripts/touchup_for_android.py $font;
+	python scripts/touchup_for_android.py $font;
 done
 
 
@@ -32,26 +32,26 @@ cp $UNHINTED_VF_PATH $HINTED_VF_PATH
 python -m vttLib mergefile sources/vtt-hinting.ttx $HINTED_VF_PATH
 python -m vttLib compile $HINTED_VF_PATH $HINTED_VF_PATH.fix --ship
 mv $HINTED_VF_PATH.fix $HINTED_VF_PATH
-python Scripts/touchup_for_web.py $HINTED_VF_PATH
-python Scripts/instantiate_statics.py $HINTED_VF_PATH fonts/hinted/static
+python scripts/touchup_for_web.py $HINTED_VF_PATH
+python scripts/instantiate_statics.py $HINTED_VF_PATH fonts/hinted/static
 for font in $(ls fonts/web/hinted/*.ttf)
 do
-	python Scripts/touchup_for_web.py $font;
+	python scripts/touchup_for_web.py $font;
 done
 
 
 # Make web
 mkdir -p fonts/web
 WEB_VF_PATH=fonts/web/Roboto[ital,wdth,wght].ttf
-python Scripts/subset_for_web.py $HINTED_VF_PATH $WEB_VF_PATH
-python Scripts/touchup_for_web.py $WEB_VF_PATH
+python scripts/subset_for_web.py $HINTED_VF_PATH $WEB_VF_PATH
+python scripts/touchup_for_web.py $WEB_VF_PATH
 # Split fonts can be removed once all browsers support slnt and ital axes properly
 mkdir -p fonts/web/split
-python Scripts/split_slnt_vf.py $WEB_VF_PATH fonts/web/split
-python Scripts/instantiate_statics.py $WEB_VF_PATH fonts/web/static
+python scripts/split_slnt_vf.py $WEB_VF_PATH fonts/web/split
+python scripts/instantiate_statics.py $WEB_VF_PATH fonts/web/static
 for font in $(ls fonts/web/static/*.ttf)
 do
-	python Scripts/touchup_for_web.py $font;
+	python scripts/touchup_for_web.py $font;
 done
 
 
@@ -63,6 +63,6 @@ python scripts/touchup_for_cros.py $CHROMEOS_VF_PATH
 python scripts/instantiate_statics.py $CHROMEOS_VF_PATH fonts/chromeos/static
 for font in $(ls fonts/chromeos/static/*.ttf)
 do
-	python Scripts/touchup_for_cros.py $font;
+	python scripts/touchup_for_cros.py $font;
 done
 
