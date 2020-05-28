0c78be20cd3dc4e23a0f37efadbb4b6856e7dc1a
Marc Foley <m.foley.88@gmail.com>
2020-05-28 16:00:30 +0100

    Cleanup touchup scripts

--- scripts/touchup_for_android.py
+++ scripts/touchup_for_android.py
@@ -4,23 +4,6 @@ from nototools import font_data
 from scripts import *
 
 
-def update_android_names(font):
-    """v2.138 Android fonts do not use typographic records (nameIDs 16, 17).
-
-    If a font contains typo records, replace nameIDs 1 and 2 with the typo
-    records."""
-    typo_family = font["name"].getName(16, 3, 1, 1033)
-    typo_style = font["name"].getName(17, 3, 1, 1033)
-
-    family = font["name"].getName(1, 3, 1, 1033)
-    style = font["name"].getName(2, 3, 1, 1033)
-
-    if typo_family:
-        font["name"].setName(typo_family.toUnicode(), 1, 3, 1, 1033)
-    if typo_style:
-        font["name"].setName(typo_style.toUnicode(), 2, 3, 1, 1033)
-
-
 def main(font_path):
     font = TTFont(font_path, recalcBBoxes=False)
     # turn off round-to-grid flags in certain problem components
@@ -35,15 +18,13 @@ def main(font_path):
         0x2191, # UPWARDS ARROW
         0x2193, # DOWNWARDS ARROW
         ])
-    # Update vertical metrics to match v2.138
+    # Update vertical metrics to match v2.136
     update_attribs(
         font,
         **android_and_cros_vert_metrics
     )
-    #update_android_names(font)
     update_psname_and_fullname(font, include_year=True)
     update_font_version(font)
-    disable_oblique_bits(font)
     font.save(font_path)
 
 
