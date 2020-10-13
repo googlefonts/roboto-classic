d04b71be59bd3b8f4fb6a45dae5bb88e62cd83e7
Marc Foley <m.foley.88@gmail.com>
2020-10-13 12:48:05 +0100

    wip: fix bad overlaps

---
+++ transfer_contours.py
@@ -0,0 +1,25 @@
+import defcon
+from glob import glob
+import os
+import shutil
+
+
+srcs = {os.path.basename(f): f for f in glob("sources/*.ufo")}
+new_paths = {os.path.basename(f): f for f in glob("sources/master_ufo/*.ufo")}
+
+missing = set(srcs.keys()) ^ set(new_paths.keys())
+if missing:
+    print('missing', missing)
+    import sys
+    sys.exit()
+
+for glyph in ["uni0472.glif", "uni0473.glif","theta.glif"]:
+    for k in new_paths:
+        new_path = os.path.join(new_paths[k], "glyphs", glyph)
+        cur_path = os.path.join(srcs[k], "glyphs", glyph)
+
+
+        print(new_path)
+        print(cur_path)
+        print()
+
