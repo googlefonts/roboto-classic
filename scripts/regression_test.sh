cb102d586304aaa9c28f57a97cf7e5c4b06aefb6
Marc Foley <m.foley.88@gmail.com>
2020-11-20 11:38:34 +0000

    CI/CD: Regression check hinted font against last tagged release

---
+++ scripts/regression_test.sh
@@ -0,0 +1,37 @@
+# Regression test generated fonts against last tagged release
+set -e
+
+mkdir -p prev_release
+
+OLD_FONT=prev_release/Roboto\[ital\,wdth\,wght\].ttf
+GENNED_FONT=fonts/hinted/Roboto\[ital\,wdth\,wght\].ttf
+
+curl -s https://api.github.com/repos/TypeNetwork/Roboto/releases/latest \
+| grep "https://github.com/TypeNetwork/Roboto/releases/download/*" \
+| cut -d ":" -f 2,3 \
+| tr -d \"\, \
+| wget -i -
+unzip -po Roboto_*.zip "hinted/Roboto\[ital\,wdth\,wght\].ttf" > $OLD_FONT
+
+
+# Diff old hinted variable font against current
+function diff {
+    diffenator $OLD_FONT $GENNED_FONT -i "$2" \
+        -html > $1/index.html \
+        -rd -r ./img/
+    mv img/ $1
+}
+
+
+mkdir -p diffs \
+	 diffs/Regular \
+	 diffs/Condensed \
+	 diffs/Italic \
+	 diffs/CondensedItalic
+
+
+diff diffs/Regular "wght=400, wdth=100"
+diff diffs/Condensed "wght=400, wdth=75"
+diff diffs/Italic "ital=1, wght=400, wdth=100"
+diff diffs/CondensedItalic "ital=1, wght=400, wdth=75"
+
